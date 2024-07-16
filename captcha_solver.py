e62632a28b6534be8c4e6da138837633de97f188
Swayam Satish <swayamsatish106@gmail.com>
2024-07-16 10:53:03 +0800

    adding a minor improvement to the captcha_solver file that helps in forming more permutations when there are 2 of the same letters

--- captcha_solver.py
+++ captcha_solver.py
@@ -120,6 +120,31 @@ class CaptchaSolver:
                 test += char
             self.mylist.append(test)
 
+    def two_same_letters(self):
+        initial_list = [char for char in self.string]
+        duplicates = []
+        for letter in self.string:
+            if self.string.count(letter) > 1 and letter not in duplicates:
+                duplicates.append(letter)
+        try:
+            for char in duplicates:
+                position_1 = initial_list.index(char)
+                initial_list.remove(initial_list[position_1])
+                position_2 = initial_list.index(char) + 1
+
+                print(f"{char} index is {position_1} and {position_2}")
+                first_half, second_half = self.string.split(self.string[position_2 - 1])
+                print(first_half)
+                print(second_half)
+                modified_first_half = first_half.replace(first_half[position_1], first_half[position_1].upper())
+                modified_second_half = second_half.replace(second_half[0], second_half[0].upper())
+                answer_1 = modified_first_half + self.string[position_2 - 1] + second_half
+                answer_2 = first_half + self.string[position_2 - 1] + modified_second_half
+                self.mylist.append(answer_1)
+                self.mylist.append(answer_2)
+        except ValueError:
+            pass
+
     def final_list(self):
         mylist = list(dict.fromkeys(self.mylist))
         return mylist
